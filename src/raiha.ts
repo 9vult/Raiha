da7c854be4077e869fb5538af3ed20c2166f69fa
25Pi25 <varunmash@outlook.com>
2023-11-07 21:09:17 -0500

    refactored raiha.ts

--- src/raiha.ts
+++ src/raiha.ts
@@ -9,21 +9,20 @@ import interactionCreate from "./listeners/interactionCreate";
 import ready from "./listeners/ready";
 import { loserboardNotify } from "./actions/loserboardNotify.action";
 import admin from 'firebase-admin'
-import { Configuration, Data, Leaderboard, Statistics, UserSettings } from './misc/types';
+import { Data } from './misc/types';
 import { DataSnapshot } from '@firebase/database-types';
 const firebase = require('../firebase.json');
 require('dotenv').config();
 
+
 export const VERSION = "2.3.5";
 
-admin.initializeApp({
+export const db = admin.initializeApp({
   credential: admin.credential.cert(firebase),
   databaseURL: process.env.DATABASE_URL
-});
+}).database();
 
-export const db = admin.database();
-
-export const leaderboards: Data = {
+export const leaderboards = {
   Native: {},
   Raiha: {},
   Loserboard: {},
@@ -32,31 +31,27 @@ export const leaderboards: Data = {
   UserSettings: {}
 } as Data;
 
-const client = new Client({
+export const CLIENT = new Client({
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent
   ]
 });
-export const CLIENT: Client = client;
 
-db.ref('/Leaderboard/Native').on("value", (data: DataSnapshot) => leaderboards.Native = data.val() as Record<string, Leaderboard>);
-db.ref('/Leaderboard/Raiha').on("value", (data: DataSnapshot) => leaderboards.Raiha = data.val() as Record<string, Leaderboard>);
-db.ref('/Leaderboard/Loserboard').on("value", function (data: DataSnapshot) {
-  const loserboard = data.val() as Record<string, Leaderboard>
+db.ref('/Leaderboard/Native').on("value", (data: DataSnapshot) => leaderboards.Native = data.val());
+db.ref('/Leaderboard/Raiha').on("value", (data: DataSnapshot) => leaderboards.Raiha = data.val());
+db.ref('/Leaderboard/Loserboard').on("value", (data: DataSnapshot) => {
+  const loserboard = data.val();
   loserboardNotify(loserboard);
   leaderboards.Loserboard = loserboard;
 });
-db.ref('/Statistics').on("value", function (data: DataSnapshot) {
-  leaderboards.Statistics = data.val() as Statistics;
-});
-db.ref('/Configuration').on("value", (data: DataSnapshot) => leaderboards.Configuration = data.val() as Record<string, Configuration>);
-db.ref('/UserSettings').on("value", (data: DataSnapshot) => leaderboards.UserSettings = data.val() as Record<string, UserSettings>);
+db.ref('/Statistics').on("value", (data: DataSnapshot) => leaderboards.Statistics = data.val());
+db.ref('/Configuration').on("value", (data: DataSnapshot) => leaderboards.Configuration = data.val());
+db.ref('/UserSettings').on("value", (data: DataSnapshot) => leaderboards.UserSettings = data.val());
 
 // Set up listeners
-client.on('ready', ready);
-client.on('interactionCreate', interactionCreate);
-client.on('messageCreate', messageCreate);
-
-client.login(process.env.TOKEN);
+CLIENT.on('ready', ready);
+CLIENT.on('interactionCreate', interactionCreate);
+CLIENT.on('messageCreate', messageCreate);
+CLIENT.login(process.env.TOKEN);
