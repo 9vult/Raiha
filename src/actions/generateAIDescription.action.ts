dd964099cf62cc47ea4752b1b5f99c95b6e22711
25Pi25 <varunmash@outlook.com>
2023-11-07 21:08:26 -0500

    updated AI description function

--- src/actions/generateAIDescription.action.ts
+++ src/actions/generateAIDescription.action.ts
@@ -1,6 +1,22 @@
 const fetch = require("node-fetch");
 
-export const generateAIDescription = async (imageUrl: string, doCaption: boolean, doOCR: boolean) => {
+interface DescriptionError {
+  error: {
+    code: number
+    message: string
+  }
+}
+interface DescriptionResults {
+  captionResult?: {
+    text: string
+    confidence: number
+  }
+  readResult?: {
+    content: string
+  }
+}
+
+export async function generateAIDescription(imageUrl: string, doCaption: boolean, doOCR: boolean) {
   const captionEndpoint = `${process.env.CV_ENDPOINT}computervision/imageanalysis:analyze?api-version=2023-02-01-preview&features=caption`;
   const ocrEndpoint = `${process.env.CV_ENDPOINT}computervision/imageanalysis:analyze?api-version=2023-02-01-preview&features=read`;
   const bothEndpoint = `${process.env.CV_ENDPOINT}computervision/imageanalysis:analyze?api-version=2023-02-01-preview&features=caption,read`;
@@ -10,36 +26,25 @@ export const generateAIDescription = async (imageUrl: string, doCaption: boolean
   else if (doCaption) endpoint = captionEndpoint;
   else endpoint = ocrEndpoint;
 
-  const payload = JSON.stringify({ url: imageUrl });
   const response = await fetch(endpoint, {
     method: 'POST',
     headers: {
       "Content-Type": "application/json",
-      "Ocp-Apim-Subscription-Key": `${process.env.CV_API_KEY}`
+      "Ocp-Apim-Subscription-Key": process.env.CV_API_KEY
     },
-    body: payload
+    body: JSON.stringify({ url: imageUrl })
   });
-  if (response.ok && doCaption && !doOCR) {
-    const result: any = await response.json();
-    return `${result['captionResult']['text']} (${result['captionResult']['confidence'].toFixed(3)})`;
-  }
-  if (response.ok && doCaption && doOCR) {
-    const result: any = await response.json();
-    const caption = `${result['captionResult']['text']} (${result['captionResult']['confidence'].toFixed(3)})`;
-    const text = result['readResult']['content'];
-    const description = `${caption}: ${text}`.replace('\n', ' \n');
-    return description;
-  }
-  if (response.ok && !doCaption && doOCR) {
-    const result: any = await response.json();
-    const text = result['readResult']['content'];
-    const description = text.replace('\n', ' \n');
-    return description;
-  }
-  try {
-    const result = await response.json();
-    return `Request failed. (${response.status}) - ${result['error']['code']}: ${result['error']['message']}`;
+
+  if (!response.ok) try {
+    const result: DescriptionError = await response.json();
+    return `Request failed. (${response.status}) - ${result.error.code}: ${result.error.message}`;
   } catch (err) {
     return `Request failed. (${response.status})`;
   }
+  
+  const result: DescriptionResults = await response.json();
+  let description = "";
+  if (doCaption) description += `${result.captionResult!.text} (${result.captionResult!.confidence.toFixed(3)})`;
+  if (doOCR) description += doCaption ? `: ${result.readResult!.content}` : result.readResult!.content;
+  return description.replace('\n', ' \n');
 }
