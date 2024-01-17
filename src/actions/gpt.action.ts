06ae9164a9818b28a4c02fd894c3e7afd90de565
9vult <ninevult@gmail.com>
2024-01-17 10:48:24 -0500

    Update prompt

--- src/actions/gpt.action.ts
+++ src/actions/gpt.action.ts
@@ -11,7 +11,11 @@ export async function Gpt(imageUrl: string) {
           content: [
             {
               type: "text",
-              text: "Please describe the contents of this image in detail using the format of a11y alt text. Please keep the response length to under 1000 characters or 300 tokens, whichever is fewer. Please do not start the response with the phrase \"Alt text\" or similar phrases. Only include the actual alt text in the response."
+              text: "Please describe the contents of this image in detail using the format of a11y alt text. " +
+                    "Please keep the response length to under 1000 characters or 300 tokens, whichever is fewer. " +
+                    "Please try to keep the response scope limited to the primary focuses of the image. " +
+                    "Please do not start the response with the phrase \"Alt text\" or similar phrases. " +
+                    "Only include the actual alt text in the response."
             },
             {
               type: "image_url",
